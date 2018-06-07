d4fd8eef8547819d88f8bba322be924bac269b9a
Francesco Pischedda <francesco.pischedda@vimcar.de>
2018-06-07 10:18:14 +0200

    fix dispatch creation function

--- src/clj/reminder/commands/dispatcher.clj
+++ src/clj/reminder/commands/dispatcher.clj
@@ -1,10 +1,10 @@
 (ns reminder.commands.dispatcher)
 
-(defn no-handler [name payload]
-  (println "No handler defined for command " name " with payload " payload)
+(defn no-handler [command payload]
+  (println "No handler defined for command " command " with payload " payload)
   {:error {:code :missing-handler}})
 
 (defn create [command-handlers]
-  (fn [{:keys [name payload]} command]
+  (fn [{:keys [name payload]}]
     (let [handler (get command-handlers name no-handler)]
       (handler name payload))))
