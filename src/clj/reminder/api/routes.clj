6f9d56df43cc0e45cad715de1554138aceb987aa
Francesco Pischedda <francesco.pischedda@vimcar.de>
2018-07-26 18:08:21 +0200

    removed reference to compojure

--- src/clj/reminder/api/routes.clj
+++ src/clj/reminder/api/routes.clj
@@ -1,6 +1,5 @@
 (ns reminder.api.routes
-  (:require [compojure.core :refer [defroutes context GET POST PATCH DELETE ANY OPTIONS]]
-            [reminder.api.auth :as auth]
+  (:require [reminder.api.auth :as auth]
             [reminder.api.v1.routes :as api_v1]
             [reminder.api.v1.reminders :as reminders]))
 
