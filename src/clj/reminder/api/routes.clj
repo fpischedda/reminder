3afcace562e6225a119aab1b990fd7bcba9ca2ca
Francesco Pischedda <francesco.pischedda@gmail.com>
2019-04-30 10:05:51 +0200

    random fixes

--- src/clj/reminder/api/routes.clj
+++ src/clj/reminder/api/routes.clj
@@ -1,8 +1,7 @@
 (ns reminder.api.routes
   (:require [bidi.bidi :refer [path-for]]
             [reminder.api.auth :as auth]
-            [reminder.api.v1.routes :as api_v1]
-            [reminder.api.v1.reminders :as reminders]))
+            [reminder.api.v1.routes :as api_v1]))
 
 
 (def routes ["/" [api_v1/routes]])
--- src/clj/reminder/api/v1/handlers.clj
+++ src/clj/reminder/api/v1/handlers.clj
@@ -1,5 +1,4 @@
 (ns reminder.api.v1.handlers
-  (:require [reminder.api.v1.reminders :as reminders]))
+  (:require [reminder.api.v1.reminders.routes :as reminders]))
 
 (def handlers ["v1/" {"reminders" [reminders/routes]}])
-
