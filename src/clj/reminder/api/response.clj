f032cbea5a8fef047ccbe03d43bbe7c578296a74
Francesco Pischedda <francesco.pischedda@gmail.com>
2019-04-30 10:06:07 +0200

    add response helper functions

---
+++ src/clj/reminder/api/response.clj
@@ -0,0 +1,33 @@
+(ns reminder.api.response
+  (:require
+   [ring.util.response :refer [response]]
+   [cheshire.core :as json]))
+
+(defn json-response [status body]
+  {:status status
+   :body (json/encode body)})
+
+(defn bad-request [& errors]
+  (json-response 400 {:errors errors}))
+
+(defn error [status & errors]
+  (json-response status {:errors errors}))
+
+(defn json-success [body]
+  (response (json/encode body)))
+
+(defn created [url]
+  {:status 201
+   :headers {"Location" url}})
+
+(defn accepted
+  ([] {:status 202})
+  ([url]
+   {:status 202
+    :headers {"Location" url}}))
+
+(defn no-content []
+  {:status 204})
+
+(defn not-found []
+  {:status 404})
