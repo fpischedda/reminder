ab9a66bf06d92efa90aad1f35f7e9a0004d57224
Francesco Pischedda <francesco.pischedda@gmail.com>
2019-05-24 15:41:21 +0200

    cleaning stuff

--- src/clj/reminder/api/server.clj
+++ src/clj/reminder/api/server.clj
@@ -1,7 +1,7 @@
 (ns reminder.api.server
   (:require [reminder.api.handlers :refer [handlers]]
             [reminder.config :refer [config]]
-            [reminder.api.authorization :refer [rules]]
+            [reminder.api :as authorization]
             [mount.core :refer [defstate]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-params]]
@@ -12,32 +12,9 @@
             [buddy.auth.accessrules :refer [wrap-access-rules]]
             [org.httpkit.server :refer [run-server]]))
 
-(defonce server-instance (atom nil))
-
 (def reminder-defaults (assoc site-defaults :security {:anti-forgery false}))
 (defn gen-auth-backend [auth-secret]
   (backends/jws {:secret auth-secret}))
-
-(def access-options {:rules rules})
-
-(defn wrap-content-type-if-not-set
-  [handler content-type]
-  (fn [request]
-    (let [response (handler request)
-          content-type (:headers "Content-Type")]
-      (if content-type
-        response
-        (assoc-in response [:headers "Content-Type"] content-type)))))
-
-(defn allow-cross-origin
-  "middleware function to allow cross origin"
-  [handler]
-  (fn [request]
-    (let [response (handler request)]
-      (-> response
-          (assoc-in [:headers "Access-Control-Allow-Origin"]  "*")
-          (assoc-in [:headers "Access-Control-Allow-Methods"] "GET,PUT,POST,PATCH,DELETE,OPTIONS")
-          (assoc-in [:headers "Access-Control-Allow-Headers"] "X-Requested-With,Content-Type,Cache-Control, Authorization")))))
 
 (defn gen-app [auth-secret]
   (let [auth-backend (gen-auth-backend auth-secret)]
@@ -45,13 +22,11 @@
         (wrap-defaults reminder-defaults)
         (wrap-keyword-params)
         (wrap-json-params)
-        (wrap-content-type-if-not-set "application/json")
-        (wrap-access-rules access-options)
+        (wrap-access-rules authorization/rules)
         (wrap-authorization auth-backend)
-        (allow-cross-origin)
         (wrap-authentication auth-backend))))
 
-
+(defonce server-instance (atom nil))
 
 (defn start []
   (let [{:keys [api-server auth-secret]} config]
