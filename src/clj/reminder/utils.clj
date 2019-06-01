c66cdc32a214e58891f02255e54d87232d5e4327
e_fpischedda <francesco.pischedda@abinsula.com>
2019-06-01 14:09:23 +0200

    get rid of the unparse method

--- src/clj/reminder/utils.clj
+++ src/clj/reminder/utils.clj
@@ -1,6 +1,5 @@
 (ns reminder.utils
   (:require [java-time :as j]
-            [clj-time.format :as f]
             [clj-uuid :as uuid]))
 
 (defn utc-now []
@@ -10,4 +9,4 @@
   (str (uuid/v1)))
 
 (defn datetime->str [datetime]
-  (f/unparse (f/formatter :basic-date-time) datetime))
+  (str datetime))
